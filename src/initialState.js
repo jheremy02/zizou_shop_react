d51ee669f0308a6bdc2f1175c01cda01d43c632a
jheremy02 <jheremy26@gmail.com>
2023-08-28 20:50:34 -0500

    se cambiaron las imagenes de producto

--- src/initialState.js
+++ src/initialState.js
@@ -1,49 +1,50 @@
 export default {
-    cart: [],
-    buyer:{},
-    orders:[],
-    products: [
-      {
-        'id': '1',
-        'image': 'https://arepa.s3.amazonaws.com/camiseta.png',
-        'title': 'Camiseta',
-        'price': 25,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-      {
-        'id': '3',
-        'image': 'https://arepa.s3.amazonaws.com/mug.png',
-        'title': 'Mug',
-        'price': 10,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-      {
-        'id': '4',
-        'image': 'https://arepa.s3.amazonaws.com/pin.png',
-        'title': 'Pin',
-        'price': 4,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-      {
-        'id': '5',
-        'image': 'https://arepa.s3.amazonaws.com/stickers1.png',
-        'title': 'Stickers',
-        'price': 2,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-      {
-        'id': '6',
-        'image': 'https://arepa.s3.amazonaws.com/stickers2.png',
-        'title': 'Stickers',
-        'price': 2,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-      {
-        'id': '7',
-        'image': 'https://arepa.s3.amazonaws.com/hoodie.png',
-        'title': 'Hoodie',
-        'price': 35,
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      },
-    ],
-  };
+  cart: [],
+  buyer: {},
+  orders: [],
+  products: [
+    {
+      id: "1",
+      image:
+        "https://cdn.pixabay.com/photo/2016/12/06/09/30/blank-1886001_640.png",
+      title: "Camiseta",
+      price: 25,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "3",
+      image: "https://cdn.pixabay.com/photo/2015/06/24/01/15/coffee-819362_640.jpg",
+      title: "Mug",
+      price: 10,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "4",
+      image: "https://images.pexels.com/photos/939331/pexels-photo-939331.jpeg?auto=compress&cs=tinysrgb&w=400",
+      title: "Lamp",
+      price: 4,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "5",
+      image: "https://cdn.pixabay.com/photo/2019/11/01/17/56/thanksgiving-4594825_640.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "6",
+      image: "https://arepa.s3.amazonaws.com/stickers2.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "7",
+      image: "https://cdn.pixabay.com/photo/2022/03/24/10/14/woman-7088803_640.jpg",
+      title: "Hoodie",
+      price: 35,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+  ],
+};
